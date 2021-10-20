2c5dcb647c86aba8bf78d3fc97ce1268b94b895d
abdullm <abdullah@d3s.mff.cuni.cz>
2021-10-20 18:08:42 +0200

    First Commit

---
+++ source/base.py
@@ -0,0 +1,9 @@
+"""
+    This file contains the base classes and formal defintions for each type that are being dealt with it in the research.\
+    The classes are:
+        1. Component Class: an abstract class that simply parnets all component objects on the field.
+            A. Drone
+            B. Charger
+            C. Bird
+
+"""
