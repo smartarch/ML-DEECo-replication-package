3ec39731953e575144f67eb9aa0fb0f160da1c0c
abdullm <abdullah@d3s.mff.cuni.cz>
2021-11-11 11:23:35 +0100

    Visualaztion Added

--- source/simulation.py
+++ source/simulation.py
@@ -1,12 +1,12 @@
 
 
 import random
-import numpy as np
+import os
 from datetime import date
-from matplotlib import pyplot as plt
-from components import Bird, Drone, Charger,Field, Point
+from components import Bird, Drone, Charger,Field, Point , Component, Agent
 from tasks import ChargerAssignment , FieldProtection
 from serializer import Report
+from visualizers import Visualizer
 currentWorld = None
 
 CLASSNAMES = {
@@ -28,8 +28,10 @@ class World:
     field : list
     gridSize: int
     maxSteps: int
-    mapWidth : int
-    mapHeight : int
+
+    # static
+    Width = 100
+    Height = 100
 
     def createComponenets(self,targetClassName,points):
         """
@@ -40,7 +42,7 @@ class World:
             targetClass = CLASSNAMES[targetClassName]
             newComponenet = targetClass(point)
             self.__dict__[targetClassName].append(newComponenet)
-
+        return self.__dict__[targetClassName]
     
 
     def __init__(self,conf_dict):
@@ -51,82 +53,116 @@ class World:
         """
         self.maxSteps = conf_dict['maxSteps']
         conf_dict['maxSteps'] =None
-
-        self.gridSize = conf_dict['gridSize']
-        conf_dict['gridSize'] =None
         
         self.mapWidth = conf_dict['mapWidth']
         conf_dict['mapWidth'] =None
-        
+        World.Width = self.mapWidth
+
         self.mapHeight = conf_dict['mapHeight']
         conf_dict['mapHeight'] =None
-
+        World.Height = self.mapHeight
+        
         self.drones = []
         self.birds =[]
         self.chargers= []
         self.fields = []
-        
+        self.map = {} 
+
+        # for x in range(self.mapWidth):
+        #     for y in range(self.mapHeight):
+        #         self.map[(x,y)] = []
+
         # creates drones, birds, chargers and fields
         for conf,conf_value in conf_dict.items():
             if conf_value is not None:
                 points = []
                 if isinstance(conf_value,int):
                     for i in range(conf_value):
-                        points.append(Point(x=random.randint(0,self.mapWidth), y =random.randint(0,self.mapHeight)))
+                        points.append([random.randint(0,self.mapWidth-1), random.randint(0,self.mapHeight-1)])
                 else:
                     points = conf_value
-                self.createComponenets(conf,points)
+                    
+                createdComponents = self.createComponenets(conf,points)
+                # add the correspoding function to the map of components
+                for component in createdComponents:
+                    self.map[component] = component.locationPoints()
+        
+        Component.World = self
+        Field.World = self
 
+    @property
+    def nonEmptyPoints(self):
+        return [point for component in self.map for point in self.map[component]]
+
+    @property
+    def emptyPoints (self):
+        return [[x,y] for x in range(self.mapWidth) for y in range(self.mapHeight) if [x,y] not in self.nonEmptyPoints]
+
+
+
+    def components(self,point):
+        if isinstance(point, Point):
+            return [component for component in self.map if [point.x,point.y] in self.map[component]]
+        else:
+            return [component for component in self.map if point in self.map[component]]
+    
+    def isPointField(self,point):
+        for field in self.fields:
+            if field.isPointOnField(point):
+                return True
+        return False
 
 class Simulation:
 
     def run (self):
-        birds = currentWorld.birds
-        birdReport = Report(Bird)
+        agents = [agent for agent in currentWorld.map if isinstance(agent, Agent)]
+        agentReporter = Report(Agent)
+
+        #droneReport= Report(Drone)
+        visualizer = Visualizer (currentWorld)
+        visualizer.drawFields()
         
-        for i in range(currentWorld.maxSteps):
-            for bird in birds:
-                bird.actuate()
-                bird.report(i)
-
-
-        today = date.today().strftime("%Y%m%d")
-        birdReport.export(f"../experiments/results/birds-{today}.csv")
-
-    def run1 (self):
-        # detailed steps
-        # create a blank list of components
-        components = []
-        components.extend(currentWorld.birds)
-        components.extend(currentWorld.drones)
-        components.extend(currentWorld.chargers)
-
-        """
-            according to the chargers
-        """
         chargerEnsembles = []
         for charger in currentWorld.chargers:
             chargerEnsembles.append (ChargerAssignment(charger))
 
-        """
-            according to fields
-        """
         fieldProtectionEnsembles= []
         for field in currentWorld.fields:
             fieldProtectionEnsembles.append (FieldProtection(field))
-
-        # in cases the priority is considered such as we say protecting > patroling
-        # TODO priority might be changed to a different feature
-        chargerEnsembles = sorted(chargerEnsembles, key=lambda x: x.priority())
-
-        # TODO know why do we have to materialize ensembles?
-        # for ens in potentialEnsembles:
-        #     if ens.materialize(components, instantiatedEnsembles):
-        #         instantiatedEnsembles.append(ens)
-        #         ens.actuate()
+        potentialEnsembles = []
+        
+        potentialEnsembles.extend (chargerEnsembles)
+        potentialEnsembles.extend (fieldProtectionEnsembles)
+        
+        
+        
+        instantiatedEnsembles = []     
+        drones = currentWorld.drones
+        for ens in potentialEnsembles:
+            if ens.materialize(drones, instantiatedEnsembles):
+                instantiatedEnsembles.append(ens)
+                ens.actuate()
+       
 
         for i in range(currentWorld.maxSteps):
-            for component in components:
-                component.actuate()
-                print (f"{i}: {component}")
+            
+            for agent in agents:
+                agent.actuate()
+                agent.report(i)
+            
+            for ens in instantiatedEnsembles:
+                ens.actuate()
+
+            visualizer.drawComponents(i+1)
+        
+        folder = "results"
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        today = date.today().strftime("%Y%m%d")
+        
+        visualizer.createAnimation(f"{folder}/simulation-{today}.gif")
+        agentReporter.export(f"{folder}/agents-{today}.csv")
+  
+
 
