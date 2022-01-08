834fd90d10ae3cb8bc9520c3865148828d6f1460
abdullm <abdullah@d3s.mff.cuni.cz>
2022-01-08 20:21:05 +0100

    Auto Average introduced

--- utils/serialization.py
+++ utils/serialization.py
@@ -22,6 +22,7 @@ class Report:
 class Log:
     def __init__(self,header):
         self.records = [ header ]
+        self.columns = len(header)
         
     def register (self,newData):
         self.records.append(newData)
@@ -31,6 +32,18 @@ class Log:
         for i in range(len(currentRecord)):
             currentRecord[i] = currentRecord[i] + newData[i]
         self.records[-1] = currentRecord
+
+    def average(self,begin= 0,end = 1):
+        assert  begin < end , "begining index must be less than ending index"
+        # ignoring first column
+        datalist = self.records[1:][begin:end]
+        count = end - begin
+        averageList = [sum([sublist[i] for sublist in datalist])/count for i in range(self.columns)]
+        return averageList
+
+
+
+
 
     def export(self,filename):
         with open(filename, 'w', newline='') as myFile:
