7dbdf053b7e1d5e47f5bd0a60732a763b0e9a839
abdullm <abdullah@d3s.mff.cuni.cz>
2022-01-08 20:20:38 +0100

    Max Damage with No Birds

--- simulation/world.py
+++ simulation/world.py
@@ -49,6 +49,7 @@ class Environment:
         self.chargerPositions = config['chargers']
         self.fieldCount = len(config['fields'])
         self.fieldPositions = config['fields']
+        self.maxDamage = config['MAX_AVERAGE_DAMAGE']
 
 
 ENVIRONMENT = Environment()
